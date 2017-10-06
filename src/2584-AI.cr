097da2aa3c6cade08761ebb548c36905be493653
Sharknevercries <sharknevercries@gmail.com>
2017-10-06 16:31:44 +0800

    Add block option.

--- src/2584-AI.cr
+++ src/2584-AI.cr
@@ -2,6 +2,7 @@ require "./*"
 require "option_parser"
 
 total = 1000
+block = 0
 player_args = ""
 evil_args = ""
 save = ""
@@ -10,7 +11,7 @@ summary = false
 OptionParser.parse! do |parser|
   parser.banner = "Usage: 2584-AI [arguments]"
   parser.on("--total=TOTAL_GAMES", "Indicate how many games to play") { |n| total = n.to_i }
-  #parser.on("--block=BLOCK", "...") { |n| block = n }
+  parser.on("--block=BLOCK", "...") { |n| block = n.to_i }
   parser.on("--play=PLAYER_ARGS", "The arguments of player initialization") { |args| player_args = args }
   parser.on("--evil=EVIL_ARGS", "The arguments of evil (environment) initialization") { |args| evil_args = args }
   #parser.on("--load=LOAD", "Specifies the name to salute") { |name| load = name }
@@ -22,7 +23,7 @@ end
 player = Player.new player_args
 evil = RandomEnvironment.new evil_args
 
-stat = Statistic.new(total)
+stat = Statistic.new(total, block)
 
 while !stat.is_finished
   stat.open_episode
